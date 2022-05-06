5156f0cf73863f7291cffdf9a74c509c584c398a
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2022-05-06 12:31:29 +0200

    new map class

---
+++ 1.Semester-Projekt/Models/Map.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1.Semester_Projekt.Models
+{
+    public class Map
+    {
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public int MapNumber { get; set; }
+    }
+}
