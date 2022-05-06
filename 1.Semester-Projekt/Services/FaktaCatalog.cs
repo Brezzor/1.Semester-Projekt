c894ce5a8d9d7e638a67d1f2db5649745d246050
Rex77pro <smaurieka@gmail.com>
2022-05-06 12:37:06 +0200

    New FaktaCatalog

---
+++ 1.Semester-Projekt/Services/FaktaCatalog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _1.Semester_Projekt.Models;
+
+namespace _1.Semester_Projekt.Services
+{
+    public class FaktaCatalog
+    {
+        private Dictionary<double, Fakta> faktas { get; }
+
+        public FaktaCatalog()
+        {
+            faktas = new Dictionary<double, Fakta>();
+            faktas.Add(1, new Fakta() { Id = 1, Emne = "Dans", Navn = "HipHop", Tekst = "HipHop er en dans" });
+        }
+
+
+        public void AddFakta(Fakta fakta)
+        {
+            if (!(faktas.ContainsKey(fakta.Id)))
+            {
+                faktas.Add(fakta.Id, fakta);
+            }
+        }
+
+        public Fakta ReedFakta(double id)
+        {
+            foreach (var fak in faktas)
+            {
+                if (fak.Key == id)
+                {
+                    return fak.Value;
+                }
+            }
+            return new Fakta();
+        }
+
+        public void UpdataFakta(Fakta fakta)
+        {
+            foreach (var fak in faktas.Values)
+            {
+                if (fak.Id == fakta.Id)
+                {
+                    fak.Id = fakta.Id;
+                    fak.Emne = fakta.Emne;
+                    fak.Navn = fakta.Navn;
+                    fak.Tekst = fakta.Tekst;
+                }
+            }
+        }
+
+    }   
+}
