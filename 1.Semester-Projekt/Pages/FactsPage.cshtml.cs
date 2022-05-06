daa847c4b73eb0edfbe17efb0a41fb1926bef43b
Brezzor <oliv095w@edu.zealand.dk>
2022-05-06 13:31:49 +0200

    Added factspages folder and tested audio player

---
+++ 1.Semester-Projekt/Pages/FactsPage.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _1.Semester_Projekt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _1.Semester_Projekt
+{
+    public class FactsPageModel : PageModel
+    {   
+        public void OnGet()
+        {
+        }
+    }
+}
