8b8e97ebeb4401538a1dfa9b34457075f36ed688
Rex77pro <smaurieka@gmail.com>
2022-05-09 11:22:37 +0200

    New

---
+++ 1.Semester-Projekt/Interfaces/IFaktaRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _1.Semester_Projekt.Models;
+
+namespace _1.Semester_Projekt.Interfaces
+{
+    public interface IFaktaRepository
+    {
+        Dictionary<int, Fakta> GetAllFakta();
+
+        void CreateFakta(Fakta fakta);
+        Fakta ReadFakta(int Id);
+        void UpdateFakta(Fakta fakta);
+        void DeleteFakta(Fakta fakta);
+        
+    }
+}
