559ce7c2cc45fd39410462c9501d4e445860f18e
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2022-05-09 11:23:21 +0200

    Created JasonFileWritter and Reader

---
+++ 1.Semester-Projekt/Helpers/JasonFileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1.Semester_Projekt.Helpers
+{
+        public class JsonFileReader
+        {
+            public static Dictionary<int, Fakta> ReadJson(string JsonFileName)
+            {
+                string jsonString = File.ReadAllText(JsonFileName);
+                return JsonConvert.DeserializeObject<Dictionary<int, Fakta>>(jsonString);
+            }
+        }
+}
---
+++ 1.Semester-Projekt/Helpers/JasonFileWritter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1.Semester_Projekt.Helpers
+{
+    public class JsonFileWritter
+    {
+        public static void WriteToJson(Dictionary<int, Fakta> faktas, string JsonFileName)
+        {
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(faktas,
+                                                               Newtonsoft.Json.Formatting.Indented);
+
+            File.WriteAllText(JsonFileName, output);
+        }
+    }
+
+}
