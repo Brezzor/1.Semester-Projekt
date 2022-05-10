670a086b40a158b2e4f4f6e3db3601c74137e8d8
Rex77pro <smaurieka@gmail.com>
2022-05-10 11:25:49 +0200

    onGet

--- 1.Semester-Projekt/Pages/FactsPages/Dans.cshtml.cs
+++ 1.Semester-Projekt/Pages/FactsPages/Dans.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _1.Semester_Projekt.Interfaces;
+using _1.Semester_Projekt.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,21 @@ namespace _1.Semester_Projekt.Pages
 {
     public class DansModel : PageModel
     {
-        public void OnGet()
+        private IFaktaRepository catalog;
+        public DansModel(IFaktaRepository repository)
         {
+            catalog = repository;
+        }
+        public Dictionary<int, Fakta> Faktas { get; private set; }
+
+        public IActionResult OnGet()
+        {
+            Faktas = catalog.GetAllFakta();
+            if (true)
+            {
+                Faktas = catalog.GetAllFakta();
+            }
+            return Page();
         }
     }
 }
