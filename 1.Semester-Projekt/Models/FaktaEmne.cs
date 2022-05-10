3d944ac5593153e858d9d498439f9ee74ca13136
Rex77pro <smaurieka@gmail.com>
2022-05-10 09:28:13 +0200

    Rename

---
+++ 1.Semester-Projekt/Models/FaktaEmne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1.Semester_Projekt.Models
+{
+    public class FaktaEmne
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public string Info { get; set; }
+    }
+}
