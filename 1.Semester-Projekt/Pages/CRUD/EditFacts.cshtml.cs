d0f3dd3a73fa9b3229c9dba88653317022045e11
Rex77pro <smaurieka@gmail.com>
2022-05-16 14:32:32 +0200

    SetUp af Edit HTML

--- 1.Semester-Projekt/Pages/CRUD/EditFacts.cshtml.cs
+++ 1.Semester-Projekt/Pages/CRUD/EditFacts.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _1.Semester_Projekt.Interfaces;
+using _1.Semester_Projekt.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,22 @@ namespace _1.Semester_Projekt.Pages
 {
     public class EditFactsModel : PageModel
     {
-        public void OnGet()
+        public Fakta Fact { get; set; }
+        private IFaktaRepository catalog;
+        public EditFactsModel (IFaktaRepository repository)
         {
+            catalog = repository;
+        }
+
+        public void OnGet() //(int id) 
+        {
+            //Fact = catalog.ReadFakta(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            catalog.UpdateFakta(Fact);
+            return RedirectToPage("Edit");
         }
     }
 }
