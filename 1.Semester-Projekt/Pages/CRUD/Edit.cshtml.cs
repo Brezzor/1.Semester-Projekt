710c43085dbd52287c192417b8c03be8b3fad6ef
Brezzor <oliv095w@edu.zealand.dk>
2022-05-17 13:19:36 +0200

    edit tables setup

--- 1.Semester-Projekt/Pages/CRUD/Edit.cshtml.cs
+++ 1.Semester-Projekt/Pages/CRUD/Edit.cshtml.cs
@@ -11,22 +11,47 @@ namespace _1.Semester_Projekt.Pages
 {
     public class EditModel : PageModel
     {
-        private IFaktaRepository catalog;
+        private const string AfspillereJsonPath = "./Data/AfspillereFakta.json";
+        private const string DansJsonPath = "./Data/DansFakta.json";
+        private const string FansJsonPath = "./Data/FansFakta.json";
+        private const string InstrumenterJsonPath = "./Data/InstrumenterFakta.json";
+        private const string KulturJsonPath = "./Data/KulturFakta.json";
+        private const string LydJsonPath = "./Data/LydFakta.json";
+        private const string LysJsonPath = "./Data/LysFakta.json";
+        private const string MusikGenreJsonPath = "./Data/MusikGenreFakta.json";
+        private const string PolitikJsonPath = "./Data/PolitikFakta.json";
+
+        private IFaktaRepository repo;
 
         public EditModel(IFaktaRepository repository)
         {
-            catalog = repository;
+            repo = repository;
         }
 
-        public Dictionary<int, Fakta> Faktas { get; private set; }
+        public Dictionary<int, Fakta> AfspillereFaktas { get; private set; }
+        public Dictionary<int, Fakta> DansFaktas { get; private set; }
+
+        public Dictionary<int, Fakta> FansFaktas { get; private set; }
+
+        public Dictionary<int, Fakta> InstrumenterFaktas { get; private set; }
+
+        public Dictionary<int, Fakta> KulturFaktas { get; private set; }
+        public Dictionary<int, Fakta> LydFaktas { get; private set; }
+        public Dictionary<int, Fakta> LysFaktas { get; private set; }
+        public Dictionary<int, Fakta> MusikGenreFaktas { get; private set; }
+        public Dictionary<int, Fakta> PolitikFaktas { get; private set; }
 
         public IActionResult OnGet()
-        {            
-            return Page();
-        }
-
-        public IActionResult OnGetOnClick()
         {
+            AfspillereFaktas = repo.GetAllFakta(AfspillereJsonPath);
+            DansFaktas = repo.GetAllFakta(DansJsonPath);
+            FansFaktas = repo.GetAllFakta(FansJsonPath);
+            InstrumenterFaktas = repo.GetAllFakta(InstrumenterJsonPath);
+            KulturFaktas = repo.GetAllFakta(KulturJsonPath);
+            LydFaktas = repo.GetAllFakta(LydJsonPath);
+            LysFaktas = repo.GetAllFakta(LysJsonPath);
+            MusikGenreFaktas = repo.GetAllFakta(MusikGenreJsonPath);
+            PolitikFaktas = repo.GetAllFakta(PolitikJsonPath);
             return Page();
         }
     }
